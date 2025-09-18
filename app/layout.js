a62d466ac0f433ac272bc2ff0475f06a8b98acf1
franciscoalliaga <franciscoalliaga@gmail.com>
2025-09-17 21:31:34 -0300

    Atualização page.js - logo, serviços, whatsapp e portfólio

--- app/layout.js
+++ app/layout.js
@@ -1,26 +1,21 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
-import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Tantum Core",
   description: "Soluções Inteligentes em Logística",
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon-32x32.png",
+    apple: "/apple-touch-icon.png",
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
-      <Head>
-        {/* Favicon principal */}
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-
-        {/* Alternativas para compatibilidade */}
-        <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32" />
-        <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
